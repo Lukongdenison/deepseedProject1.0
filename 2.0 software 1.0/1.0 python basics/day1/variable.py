fdcfa9797b86d938cb6d4b2f565cfac09e9b5fc6
Denison <dennisonlukong@gmail.com>
2025-07-23 06:39:33 -0700

    done with varaibles

---
+++ 2.0 software 1.0/1.0 python basics/day1/variable.py
@@ -0,0 +1,63 @@
+# # print("hello deepseeds")
+# # #why do we need variables?
+# # name="cake"
+# # print("I am Gita, i love the name Gita, Gita was given to me by my fathe Paul Gita and i really love the name Gita ")
+
+# # print ("my name is " + name + "i love " + name + "was givenby my father, " + name + "paul")
+# # whole_number =42
+# # decimal_num = 3.14
+# # complex_num = 2 + 3j
+
+# # greeting = "hello"
+# # single_char = 'a'
+
+# # is_summary = True
+# # is_summary = False
+# # print(type (whole_number))
+# # print(type(greeting))
+
+# # #ariables basic example 
+# # # concatenate variables 
+# # first_name = "John"
+# # last_name = "Doe"
+# # combine_name=first_name + last_name
+# # print("my name is {combine_name}")
+
+
+# #A ban mame generator
+
+# name=input("What is your name?: ")
+# print (f"my name is {name}")
+
+# age=input("What is your age?: ")
+# print(f"My age is {age}")de
+
+# fav_meal=input("What is your favorite meal?: ")
+# print(f"My favorite meal is {fav_meal}")
+
+# school=input("What is your school?: ")
+# print(f"My school is {school}")
+
+# department=input("What is your department?: ")
+# print(f"My department is {department}")
+
+# best_friend=input("What is your best friend's name?: ")
+# print(f"My best friend's name is {best_friend}")
+
+# print( f"Hello, my name is {name}, I am {age} years old, my favorite meal is {fav_meal}, I attend {school} in the {department} department, and my best friend's name is {best_friend}.")
+# # This code collects user information and prints a formatted introduction
+# # It prompts the user for their name, age, favorite meal, school, department, and best friend's name
+
+#exercise
+#create a calculator for a rectangle
+length=10
+width=5
+area=length*width
+perimeter=2*(length+width)
+print(f"The perimeter of the rectangle is {perimeter}") 
+print(f"The area of the rectangle is {area}")
+
+#temperature conversion
+temperature=input("Enter the temperature in Celsius: ")
+fahrenheit=(float(temperature) * 9/5) + 32
+print(f"The temperature in Fahrenheit is {fahrenheit}")
